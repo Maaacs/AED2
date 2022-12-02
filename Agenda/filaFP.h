6df058464ff63c94c15352d0b12b2f32c7f10949
Maaacs <msb@icomp.ufam.edu.br>
2022-12-02 16:32:06 -0400

    funcoes da Fila de Prioridade em filaFP.h

---
+++ Agenda/filaFP.h
@@ -0,0 +1,8 @@
+typedef struct fila TFila;
+
+TFila* criar_FP(int tamanho);
+void enfileirar_FP (TFila *f, void* elem);
+void* desenfileirar_FP (TFila* f);
+int tamanho_fila (TFila* f);
+destroy_FP();
+
