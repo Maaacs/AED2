5f0143eef7a9c09fe7d2c6dd695a0989604f289b
Maaacs <msb@icomp.ufam.edu.br>
2022-12-03 08:47:51 -0400

    struc elemento e criar_agenda e agendar_evento

--- Agenda/agenda.h
+++ Agenda/agenda.h
@@ -1,3 +1,8 @@
-typedef struct agenda TAgenda; //tipo agenda
-TAgenda* criar_agenda(char *titulo, char *diaMesAno, char *hora, char *local, char *descricao);
-void imprimir_agenda(TAgenda* l);
+typedef struct agendaInfo TipoAgendaInfo; //tipo agenda
+typedef struct elementoLSE TipoAgendaLSE;
+
+TipoAgendaLSE* criar_agenda();
+
+TipoAgendaLSE* agendar_evento(char *titulo, char *data, int tempolimite, char *local, char *descricao);
+
+//void imprimir_agenda(TAgenda* l);
