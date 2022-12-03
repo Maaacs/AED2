6eb67d5ef9a94f2345d11a24534a600583596edb
Maaacs <msb@icomp.ufam.edu.br>
2022-12-03 08:46:22 -0400

    criar_agenda e agendar_evento

--- Agenda/agenda.c
+++ Agenda/agenda.c
@@ -4,34 +4,52 @@
 #include "agenda.h"
 
 
-struct agenda{
+// struct agenda{
+//     char data[8];
+//     char hora[5];
+//     int tempoLimite; //tempo limite para a realização do evento desde o agendamento em segundos
+//     int prioridade; //de 1 a 4
+// };
+struct agendaInfo{
     char titulo[60];
-    char diaMesAno[8];
-    char hora[5];
+    char data[8]; // dia/mes/ano
+    int tempoLimite; //tempo limite para a realização do evento desde o agendamento em segundos
     char local[20];
     char descricao[30];
 };
 
-//typedef struct agenda TAgenda; //tipo agenda
+struct elementoLSE{
+    TipoAgendaInfo* cargaUtil;
+    int deadline;
+};
 
-TAgenda* criar_agenda(char *titulo, char *diaMesAno, char *hora, char *local, char *descricao){
-    TAgenda *l = malloc(sizeof(TAgenda));
 
-    strcpy(l->titulo, titulo); //tem que passar cada char do vetor de char, por isso fazer uma copia com strcpy
-    strcpy(l->diaMesAno, diaMesAno);
-    strcpy(l->hora, hora);
-    strcpy(l->local, local);
-    strcpy(l->descricao, descricao);
+TipoAgendaLSE* criar_agenda(){
 
+    TipoAgendaLSE *l = malloc(sizeof(TipoAgendaLSE)); // aloca na memória o elementoLSE que contem os campos: cargaUtil e deadline
+    l->deadline = 0;
     return l;
 }
 
-// void imprimir_agenda(TAgenda* l){
-//      printf("%s %s %s %s %s\n", l->titulo, l->diaMesAno, l->hora, l->local, l->descricao);
-// }
 
-agendar_evento();
-proximo_evento();
-remover_evento();
+TipoAgendaLSE* agendar_evento(char *titulo, char *data, int tempolimite, char *local, char *descricao){ //insere um novo evento na agenda considerando os atributos que irão posicionar o evento na agenda
+
+    TipoAgendaLSE *l;
+
+    strcpy(l->cargaUtil->titulo, titulo); //tem que passar cada char do vetor de char, por isso fazer uma copia com strcpy
+    strcpy(l->cargaUtil->data, data);
+    l->cargaUtil->tempoLimite = tempolimite;
+    strcpy(l->cargaUtil->local, local);
+    strcpy(l->cargaUtil->descricao, descricao);
+
+    if (l->cargaUtil->tempoLimite < l->deadline){
+        l->deadline = tempolimite; // grava o menor tempo sempre
+    }
+
+}
+proximo_evento(); // retorna o evento que está na eminência de ocorrer
+
+remover_evento(); // remove e retorna o evento que está na eminência de ocorrer.
+
 
 
