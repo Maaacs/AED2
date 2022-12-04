2db4155b106b3d21a40f8dcd191c96e7d419dec8
Maaacs <msb@icomp.ufam.edu.br>
2022-12-04 16:13:53 -0400

    Adicionando as funcoes da agenda

--- Agenda/agenda.h
+++ Agenda/agenda.h
@@ -3,5 +3,5 @@ typedef struct elementoLSE TipoAgendaLSE;
 
 TipoAgendaLSE* criar_agenda();
 TipoAgendaLSE* agendar_evento(char *titulo, char *data, int tempolimite, char *local, char *descricao, int Prioridade);
-// TipoAgendaLSE* agendar_evento(TipoAgendaInfo* carga, int Prioridade);
-//void imprimir_agenda(TAgenda* l);
+TipoAgendaInfo* proximo_evento();
+TipoAgendaInfo* remover_evento();
