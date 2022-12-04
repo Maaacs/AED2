65d20c0462f1f7af1cca6d607787c508f7bb3450
Maaacs <msb@icomp.ufam.edu.br>
2022-12-04 18:43:27 -0400

    Destroy

--- Agenda/filaFP.h
+++ Agenda/filaFP.h
@@ -5,5 +5,5 @@ typedef struct filaPrioridade TFilaPrioridade;
 TFilaPrioridade* criar_FP(int tamanho, TCompararFP comparar);
 void enfileirar_FP (TFilaPrioridade *f, void* elem);
 void* desenfileirar_FP (TFilaPrioridade* f);
-//destroy_FP();
+void destroy_FP(TFilaPrioridade* f);
 
