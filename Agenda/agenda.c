d5f61d9fcb17a343d74912cd874ca8f17c7c3f86
Maaacs <msb@icomp.ufam.edu.br>
2022-12-05 19:40:43 -0400

    Adicionando e atualizando campo prioridade aos eventos

--- Agenda/agenda.c
+++ Agenda/agenda.c
@@ -4,6 +4,7 @@
 #include "agenda.h"
 
 struct agendaInfo{
+    int prioridade;
     char titulo[60];
     char data[8]; // dia/mes/ano
     int tempoLimite; //tempo limite para a realização do evento desde o agendamento em segundos
@@ -15,33 +16,35 @@ struct elementoLSE{
     TipoAgendaInfo* cargaUtil; //agendaInfo
     TipoAgendaInfo *cargaMenorTempo;
     TipoAgendaInfo *cargaMenorTempoAuxiliar;
-    int prioridade;
+    int Prioridade;
     int deadline;
 };
 
 
 TipoAgendaLSE* criar_agenda(){
     TipoAgendaLSE *l = malloc(sizeof(TipoAgendaLSE)); // aloca na memória o elementoLSE com seus campos acima
-    l->cargaMenorTempo = l->cargaUtil;
-    l->prioridade = 1; // valor baixo (prioridade vai de 1 a 4)
+    l->cargaMenorTempo = l->cargaUtil; // endereco para o elemento
+    l->Prioridade = 1; // valor baixo (prioridade vai de 1 a 4)
     l->deadline = 2419200; // Um tempo alto de 4semanas==672h==40320min==249200s
     return l;
 }
 
-TipoAgendaLSE* agendar_evento(char *titulo, char *data, int tempolimite, char *local, char *descricao, int Prioridade){//insere um novo evento na agenda considerando os atributos que irão posicionar o evento na agenda
+TipoAgendaLSE* agendar_evento(int Prioridade, char *titulo, char *data, int tempolimite, char *local, char *descricao){//insere um novo evento na agenda considerando os atributos que irão posicionar o evento na agenda
 //TipoAgendaLSE* agendar_evento(TipoAgendaInfo* carga, int Prioridade){ 
-    TipoAgendaLSE *l;    
+    TipoAgendaLSE *l;  
+    l->cargaUtil->prioridade = Prioridade;  
     strcpy(l->cargaUtil->titulo, titulo); //tem que passar cada char do vetor de char, por isso fazer uma copia com strcpy
     strcpy(l->cargaUtil->data, data);
     l->cargaUtil->tempoLimite = tempolimite;
     strcpy(l->cargaUtil->local, local);
     strcpy(l->cargaUtil->descricao, descricao);
-    l->prioridade = Prioridade; //podemos tratar a prioridade como um elemento da LSE ou uma informaçao interna da AgendaInfo
 
-    if (l->cargaUtil->tempoLimite < l->deadline){
+    if ((l->cargaUtil->tempoLimite < l->deadline) && (Prioridade >= l->Prioridade)){ // menor tempo e maior prioridade
         l->deadline = l->cargaUtil->tempoLimite; // grava o menor tempo sempre
         l->cargaMenorTempoAuxiliar = l->cargaMenorTempo; // endereco do evento com o segundo menor tempo
         l->cargaMenorTempo = l->cargaUtil; //endereco do evento com o menor tempo
+
+        l->Prioridade = Prioridade; //grava a nova maior prioridade
     }
 
     return l;
@@ -50,21 +53,24 @@ TipoAgendaLSE* agendar_evento(char *titulo, char *data, int tempolimite, char *l
 
 TipoAgendaInfo* proximo_evento(){ // retorna o evento que está na eminência de ocorrer
     TipoAgendaLSE *l; 
-    return l->cargaMenorTempo; //já tempos um campo que contém essa informação
+    return l->cargaMenorTempo; //já temos um campo que contém essa informação então só retornar o endereco desse evento
 }
 
-// EStou levando em consideraçao apenas o tempo. Devo levar em consideraçao a prioridade será? Talvez n pq isso é uma agenda. 
-// Prioridade serviria somente lá para a fila de prioridade na hora de organizar os elementos no vetor
+// Estou levando em consideraçao apenas o tempo. Devo levar em consideraçao a prioridade será? Talvez n pq isso é uma agenda. 
+// Prioridade serviria somente lá para a fila de prioridade na hora que ela for organizar os elementos no vetor.
 TipoAgendaInfo* remover_evento(){ // remove e retorna o evento que está na eminência de ocorrer. (mantendo sempre o menor tempo (o segundo menor tempo))
     TipoAgendaLSE *l;
     TipoAgendaInfo *aux;
-
     aux = l->cargaMenorTempo; // aux recebe o endereco do evento com o menor tempo
     l->cargaMenorTempo = l->cargaMenorTempoAuxiliar; // menor tempo recebe o endereco do segundo menor tempo
     free(aux); // remove o evento com o menor tempo
-
     return aux;
 }
+
+
+// void imprimir_eventoAgenda(TipoAgendaInfo* l){
+//     printf("%d %s %s %d %s %s\n", l->prioridade, l->titulo, l->data, l->tempoLimite, l->local, l->descricao);
+// }
 
 
 
--- Agenda/agenda.h
+++ Agenda/agenda.h
@@ -2,6 +2,7 @@ typedef struct agendaInfo TipoAgendaInfo; //tipo agenda
 typedef struct elementoLSE TipoAgendaLSE;
 
 TipoAgendaLSE* criar_agenda();
-TipoAgendaLSE* agendar_evento(char *titulo, char *data, int tempolimite, char *local, char *descricao, int Prioridade);
+TipoAgendaLSE* agendar_evento(int Prioridade, char *titulo, char *data, int tempolimite, char *local, char *descricao);
 TipoAgendaInfo* proximo_evento();
 TipoAgendaInfo* remover_evento();
+//void imprimir_eventoAgenda(TipoAgendaInfo* l);
