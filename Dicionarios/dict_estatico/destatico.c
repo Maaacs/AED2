5d80c9a19a1afe881cfae50fa1f15f009fe9b2f3
Maaacs <msb@icomp.ufam.edu.br>
2022-12-12 10:54:10 -0400

    criando structs dicionario estatico

---
+++ Dicionarios/dict_estatico/destatico.c
@@ -0,0 +1,20 @@
+#include "destatico.h"
+
+typedef struct entrada{
+    int chave;
+    void* info;
+}TEntradaDic;
+
+typedef struct destatico{
+    TEntradaDic entradas[100];
+    int tamanho;
+    int ocupacao;
+}TDEstatico;
+
+// definindo as operacoes sobre o conjunto acima
+
+TDEstatico* criar_DE(); // significa criar uma instancia do TDEstatico
+void* buscar_DE(TDEstatico*, int chave);
+int ocupacao_DE(TDEstatico*);
+
+void carga_DE(TDEstatico*);
